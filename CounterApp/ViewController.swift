8d2a4a1aab699a12de0878d05a163f306105a9c7
Ацамаз Бицоев <atsamazbitsoev@yandex.ru>
2021-11-13 15:25:12 +0300

    Initial Commit

---
+++ CounterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CounterApp
+//
+//  Created by Ацамаз Бицоев on 13.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
